5c3f1b9498d44010e127c1d6f6a463bc04ff879f
Marcos Tanaka <marcos.tanaka@db1.com.br>
2017-09-12 18:01:58 -0300

    Updating to Java 8

--- src/main/java/br/com/marcosatanaka/mybatismappergenerator/MyBatisMapperGenerator.java
+++ src/main/java/br/com/marcosatanaka/mybatismappergenerator/MyBatisMapperGenerator.java
@@ -1,4 +1,4 @@
-package generator;
+package br.com.marcosatanaka.mybatismappergenerator;
 
 import javax.persistence.Column;
 import javax.persistence.Id;
@@ -7,14 +7,11 @@ import javax.persistence.Transient;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class MyBatisMapperGenerator {
 
-    private static final Class<YOUR_CLASS_HERE> CLAZZ = YOUR_CLASS_HERE.class;
+    private static final Class<Object> CLAZZ = Object.class;
 
     private static final String NOT_IDENTIFIED = "#PROPERTY_NOT_IDENTIFIED#";
     private static final String LIST_ELEMENT_TYPE = "#ListElementType#";
@@ -29,32 +26,26 @@ public class MyBatisMapperGenerator {
     private static final String TAG_SELECT = "<select id=\"%s\" resultMap=\"resultMap%s\">" + NEW_LINE + "</select>";
     private static final String TAG_COLLECTION = "<collection property=\"%s\" javaType=\"ArrayList\" ofType=\"" + LIST_ELEMENT_TYPE + "\" select=\"findAll%s\" column=\"id\"/>";
 
-    private static final List<String> SUBSELECTS = new ArrayList<String>();
+    private static final List<String> SUBSELECTS = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println(String.format(TAG_RESULT_MAP_OPEN, CLAZZ.getSimpleName(), CLAZZ.getSimpleName()));
 
         Field[] classFields = CLAZZ.getDeclaredFields();
 
-        List<Field> resultOrIDFields = new ArrayList<Field>();
-        List<Field> associationFields = new ArrayList<Field>();
-        List<Field> collectionFields = new ArrayList<Field>();
+        List<Field> resultOrIDFields = new ArrayList<>();
+        List<Field> associationFields = new ArrayList<>();
+        List<Field> collectionFields = new ArrayList<>();
 
-        for (Field field : classFields) {
-            if (field.getType().equals(String.class)
-                    || field.getType().equals(BigDecimal.class)
-                    || field.getType().equals(Integer.class) || field.getType().equals(Integer.TYPE)
-                    || field.getType().equals(Date.class) || field.getType().equals(Double.class)
-                    || field.getType().equals(Long.class) || field.getType().equals(Long.TYPE)
-                    || field.getType().equals(Boolean.class) || field.getType().equals(Boolean.TYPE)
-                    || field.getType().isEnum()) {
-                resultOrIDFields.add(field);
-            } else if (field.getType().equals(List.class) || field.getType().equals(Set.class)) {
-                collectionFields.add(field);
-            } else {
-                associationFields.add(field);
-            }
-        }
+		Arrays.stream(classFields).forEach(field -> {
+			if (isValue(field)) {
+				resultOrIDFields.add(field);
+			} else if (isCollection(field)) {
+				collectionFields.add(field);
+			} else {
+				associationFields.add(field);
+			}
+		});
 
         createTagResultAndId(resultOrIDFields);
         createTagAssociation(associationFields);
@@ -65,54 +56,54 @@ public class MyBatisMapperGenerator {
         createSubselects(SUBSELECTS);
     }
 
-    private static void createTagResultAndId(List<Field> resultOrIDFields) {
-        for (Field field : resultOrIDFields) {
+	private static boolean isValue(Field field) {
+		return field.getType().equals(String.class)
+				|| field.getType().equals(BigDecimal.class)
+				|| field.getType().equals(Integer.class) || field.getType().equals(Integer.TYPE)
+				|| field.getType().equals(Date.class) || field.getType().equals(Double.class)
+				|| field.getType().equals(Long.class) || field.getType().equals(Long.TYPE)
+				|| field.getType().equals(Boolean.class) || field.getType().equals(Boolean.TYPE)
+				|| field.getType().isEnum();
+	}
+
+	private static boolean isCollection(Field field) {
+		return field.getType().equals(List.class) || field.getType().equals(Set.class);
+	}
+
+	private static void createTagResultAndId(List<Field> resultOrIDFields) {
+        resultOrIDFields.forEach(field -> {
             if (idAnnotation(field.getDeclaredAnnotations())) {
                 System.out.println(String.format(TAG_ID, field.getName(), getColumnName(field.getDeclaredAnnotations())));
             } else if (!isTransient(field.getDeclaredAnnotations()) && field.getDeclaredAnnotations().length > 0) {
                 System.out.println(String.format(TAG_RESULT, field.getName(), getColumnName(field.getDeclaredAnnotations())));
             }
-        }
+        });
     }
 
     private static void createTagAssociation(List<Field> associationFields) {
-        for (Field field : associationFields) {
-            if (!isTransient(field.getDeclaredAnnotations())) {
-                String subselectName = String.format(SUBSELECT_NAME, field.getType().getSimpleName());
-                System.out.println(String.format(TAG_ASSOCIATION, field.getName(), getColumnName(field.getDeclaredAnnotations()), field.getType().getSimpleName(), subselectName));
-                SUBSELECTS.add(String.format(TAG_SELECT, subselectName, field.getType().getSimpleName()));
-            }
-        }
+        associationFields.stream()
+                .filter(field -> !isTransient(field.getDeclaredAnnotations()))
+                .forEach(field -> {
+                    String subselectName = String.format(SUBSELECT_NAME, field.getType().getSimpleName());
+                    System.out.println(String.format(TAG_ASSOCIATION, field.getName(), getColumnName(field.getDeclaredAnnotations()), field.getType().getSimpleName(), subselectName));
+                    SUBSELECTS.add(String.format(TAG_SELECT, subselectName, field.getType().getSimpleName()));
+                });
     }
 
     private static void createTagCollection(List<Field> collectionFields) {
-        for (Field field : collectionFields) {
-            System.out.println(String.format(TAG_COLLECTION, field.getName(), field.getName()));
-        }
+        collectionFields.forEach(field -> System.out.println(String.format(TAG_COLLECTION, field.getName(), field.getName())));
     }
 
     private static void createSubselects(List<String> subSelects) {
-        for (String select : subSelects) {
-            System.out.println(NEW_LINE + select);
-        }
+        subSelects.forEach(select -> System.out.println(String.format("%s%s", NEW_LINE, select)));
     }
 
     private static Boolean isTransient(Annotation[] annotations) {
-        for (Annotation annotation : annotations) {
-            if (annotation instanceof Transient) {
-                return true;
-            }
-        }
-        return false;
+        return Arrays.stream(annotations).anyMatch(Transient.class::isInstance);
     }
 
     private static boolean idAnnotation(Annotation[] annotations) {
-        for (Annotation annotation : annotations) {
-            if (annotation instanceof Id) {
-                return true;
-            }
-        }
-        return false;
+        return Arrays.stream(annotations).anyMatch(Id.class::isInstance);
     }
 
     private static String getColumnName(Annotation[] annotations) {
